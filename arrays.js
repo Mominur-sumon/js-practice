48f620248b60c835124ad6bbc37c82df4721eb0e
Md. Mominur Rahman <mominur.sumon1@gmail.com>
2024-08-28 22:30:13 +0600

    json basics

--- arrays.js
+++ arrays.js
@@ -106,63 +106,130 @@ const barries = ['strawberry', 'blueberry', 'blackberry'];
 // delete person.age;
 // console.log(person);
 
-let x ;
-// const todo = {};
+// let x ;
+// // const todo = {};
 
-const todo = new Object();
-todo.id = 1 ;
-todo.name = 'Learn JavaScript';
-todo.isCompleted = true;
+// const todo = new Object();
+// todo.id = 1 ;
+// todo.name = 'Learn JavaScript';
+// todo.isCompleted = true;
 
-x = todo;
+// x = todo;
 
-const person = {
-    adress : {
-        city: 'Dhaka',
-        country: 'Bangladesh',
-    }
-}
-x = person.adress.city;
+// const person = {
+//     adress : {
+//         city: 'Dhaka',
+//         country: 'Bangladesh',
+//     }
+// }
+// x = person.adress.city;
 
-const obj1 = {
-    a: 1,
-    b: 2,
-}
-const obj2 = {
-    d: 3,
-    e: 4,
-}
+// const obj1 = {
+//     a: 1,
+//     b: 2,
+// }
+// const obj2 = {
+//     d: 3,
+//     e: 4,
+// }
 
 
-const obj3 ={obj1, obj2};
-const obj4 = {...obj1, ...obj2};
-const obj5 = Object.assign({},  obj1, obj2);
+// const obj3 ={obj1, obj2};
+// const obj4 = {...obj1, ...obj2};
+// const obj5 = Object.assign({},  obj1, obj2);
 
-const tudos = [
-    {
-        id: 1,
-        name: 'Learn JavaScript',
-        isCompleted: true,
-    },
-    {
-        id: 2,
-        name: 'Learn React',
-        isCompleted: false,
-    },
-    {
-        id: 3,
-        name: 'Learn Node',
-        isCompleted: false,
-    },
-]
+// const tudos = [
+//     {
+//         id: 1,
+//         name: 'Learn JavaScript',
+//         isCompleted: true,
+//     },
+//     {
+//         id: 2,
+//         name: 'Learn React',
+//         isCompleted: false,
+//     },
+//     {
+//         id: 3,
+//         name: 'Learn Node',
+//         isCompleted: false,
+//     },
+// ]
 
-x = obj3;
-x = obj4;
-x = obj5;
-x = tudos ;
-x = Object.keys(tudos);
-x = Object.values(tudos);
-x = Object.entries(tudos);
-x = tudos.length;
-x = tudos.hasOwnProperty('id');
-console.log(x);
+// x = obj3;
+// x = obj4;
+// x = obj5;
+// x = tudos ;
+// x = Object.keys(tudos);
+// x = Object.values(tudos);
+// x = Object.entries(tudos);
+// x = tudos.length;
+// x = tudos.hasOwnProperty('id');
+// console.log(x);
+
+// const firstName = 'Md Mominur';
+// const lastName = 'Rahman';
+// const age = 30;
+// const person = {
+//     firstName,
+//     lastName,
+//     age,
+// };
+// console.log(person);
+
+// Object Destructuring
+
+// const tudo ={
+//     id: 1,
+//     name: 'Learn JavaScript',
+//     isCompleted: true,
+// }
+
+// const {id, name, isCompleted} = tudo;
+// console.log(id, name, isCompleted);
+
+// // Distructure arrays
+// const arr2 = [1, 2, 3, 4, 5];
+// const  [first, second] = arr2;
+
+// console.log(first, second);
+// const [first1 , second1, ...rest] = arr2;
+// console.log(first1, second1, rest);
+
+// const post = {
+//     id: 3,
+//     nome: 'José',
+//     idade: 35,
+//     cidade: 'Curitiba'
+// }
+// // convert tp json string
+
+// const str = JSON.stringify(post);
+
+// // convert to json object
+// const obj = JSON.parse(str);
+// console.log(str);
+// console.log(obj);
+
+// const post1 = [
+//     {
+//         id: 1,
+//         nome: 'José',
+//         idade: 35,
+//         cidade: 'Curitiba'
+//     },
+//     {
+//         id: 2,
+//         nome: 'Maria',
+//         idade: 25,
+//         cidade: 'São Paulo'
+//     },
+//     {
+//         id: 3,
+//         nome: 'João',
+//         idade: 45,
+//         cidade: 'Rio de Janeiro'
+//     }
+// ]
+
+// const str1 = JSON.stringify(post1);
